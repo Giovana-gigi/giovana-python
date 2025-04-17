22cd62af987e5ec27aa4791dde5d887efe204a7d
Giovana Esmelardi <gigi.coneg09@gmail.com>
2025-04-17 14:56:09 -0300

    Create math.py
    Matemática

---
+++ math.py
@@ -0,0 +1,31 @@
+# Caso preciser de uma ajuda na matemática em Python, segue mue código
+
+
+
+import os
+os.system('cls')
+
+adicao = 10 + 10   # inteiro
+print('Adição', adicao)
+
+subtracao = 10 - 5 # inteiro
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10 # inteiro
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # inteiro
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10 # 2 elevado a 10    
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão, é par ou ímpar (0 ou 1)
+print('Módulo', modulo)
+
+print(10 % 10 ==  0) # 10 é divisível por 10?
+print(10 % 8 ==  0) # 10 é divisível por 10?
+print(10 % 3 ==  0) 
